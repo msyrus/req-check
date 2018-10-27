2adc5e8752efa465d61418bf45e053e8d27aeca7
Minhaz Ahmed Syrus <msyrus@gmail.com>
2018-10-27 19:37:32 +0600

    dump output into file

--- cmd/req-check/main.go
+++ cmd/req-check/main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"flag"
 	"io"
+	"log"
 	"mime"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -16,14 +18,68 @@ const MiB = 1 << (10 * 2)
 
 var (
 	port, bcap int
+	ch         chan interface{}
 )
 
 func main() {
 	flag.IntVar(&port, "port", 8080, "server port")
 	flag.IntVar(&bcap, "cap", 512, "max request body bytes in response")
+
 	flag.Parse()
 
+	out := flag.Arg(0)
+	if out == "" {
+		log.Fatal("output file is required")
+	}
+
+	inf, err := os.Stat(out)
+	if err != nil && !os.IsNotExist(err) {
+		catch(err)
+	}
+	if inf.IsDir() {
+		log.Fatal(out + " is a directory")
+	}
+
+	file, err := os.Create(out)
+	catch(err)
+	defer file.Close()
+
+	ch = make(chan interface{}, 100)
+	done := make(chan struct{}, 0)
+
+	go func() {
+		i := 0
+		_, err := file.WriteString("[")
+		catch(err)
+
+		enc := json.NewEncoder(file)
+
+		for data := range ch {
+			if i != 0 {
+				_, err = file.WriteString(",")
+				catch(err)
+			}
+			err = enc.Encode(data)
+			catch(err)
+
+			i++
+		}
+
+		_, err = file.WriteString("]")
+		catch(err)
+
+		done <- struct{}{}
+	}()
+
 	http.ListenAndServe(":"+strconv.Itoa(port), http.HandlerFunc(HandleReq))
+	close(ch)
+	<-done
+}
+
+func catch(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // HandleReq serves req info
@@ -57,6 +113,10 @@ func HandleReq(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 
 	json.NewEncoder(w).Encode(resp)
+
+	go func(v map[string]interface{}) {
+		ch <- v
+	}(resp)
 }
 
 func parseBody(r *http.Request) (interface{}, bool, error) {
